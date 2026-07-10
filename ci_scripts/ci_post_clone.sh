3cb52b5249d5e0d133c83513d25f558eccec644c
Kyle Kauffman <code@kyonifer.com>
2026-07-09 20:19:04 -0400

    Strip unused resources from builds.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,8 +4,13 @@ set -e
 echo "Installing dependencies..."
 brew install xcodegen imagemagick
 
-echo "Generating icons..."
 cd "$CI_PRIMARY_REPOSITORY_PATH"
+
+echo "Removing unused Foliate PDF support..."
+rm -f SilveranKit/Sources/AppleKit/Resources/WebResources/foliate-js/pdf.js
+rm -rf SilveranKit/Sources/AppleKit/Resources/WebResources/foliate-js/vendor/pdfjs
+
+echo "Generating icons..."
 ./scripts/genicons
 
 echo "Generating Xcode project..."
