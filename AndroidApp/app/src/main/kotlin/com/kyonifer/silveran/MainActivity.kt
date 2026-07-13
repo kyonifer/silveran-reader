5a41c418c0a148ad9d91f55e06f7b34f72abcd40
Kyle Kauffman <code@kyonifer.com>
2026-07-12 21:44:03 -0400

    Clean abandoned Swift download temp files on Android.

--- AndroidApp/app/src/main/kotlin/com/kyonifer/silveran/MainActivity.kt
+++ AndroidApp/app/src/main/kotlin/com/kyonifer/silveran/MainActivity.kt
@@ -11,8 +11,16 @@ import com.kyonifer.silveran.ui.SilveranViewModel
 import java.io.File
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        private var didCleanDownloadTemps = false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!didCleanDownloadTemps) {
+            cleanupAbandonedSwiftDownloads(cacheDir)
+            didCleanDownloadTemps = true
+        }
         configureSwiftNetworking(filesDir)
         enableEdgeToEdge()
 
@@ -22,6 +30,18 @@ class MainActivity : ComponentActivity() {
         )[SilveranViewModel::class.java]
 
         setContent { SilveranApp(viewModel) }
+    }
+}
+
+private val swiftDownloadTempName =
+    Regex("""^[0-9A-Fa-f]{8}(?:-[0-9A-Fa-f]{4}){3}-[0-9A-Fa-f]{12}(?:\.tmp)?$""")
+
+private fun cleanupAbandonedSwiftDownloads(cacheDirectory: File) {
+    // FoundationNetworking leaves UUID temp files after cancellation or process death.
+    cacheDirectory.listFiles()?.forEach { file ->
+        if (file.isFile && swiftDownloadTempName.matches(file.name)) {
+            file.delete()
+        }
     }
 }
 
